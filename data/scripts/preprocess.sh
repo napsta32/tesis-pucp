feff097715334912fed717db87b45e49978476d0
francopc <francopc@wolfram.com>
2022-07-23 23:55:37 -0500

    Move files

--- data/scripts/preprocess.sh
+++ data/scripts/preprocess.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-rm -rf /data/h36m/raw
 mkdir -p /data/h36m/raw
 
 echo "raw folder"
 ls /data/h36m | grep .tgz | while read line; do echo "$line hereee";done
+ls /data/h36m | grep .tgz | while read line
+do
+    mv /data/h36m/${line} /data/h36m/raw/${line}
+done
 # mv /data/h36m/** /data/h36m/raw
