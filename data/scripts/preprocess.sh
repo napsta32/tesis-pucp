bbf74bad1e1c484a094350680ad38a37056db14c
francopc <francopc@wolfram.com>
2022-07-23 23:26:20 -0500

    Fix file eol

--- data/scripts/preprocess.sh
+++ data/scripts/preprocess.sh
@@ -3,7 +3,6 @@
 rm -rf /data/h36m/raw
 mkdir -p /data/h36m/raw
 
-ls /data/h36m
 echo "raw folder"
-ls /data/h36m | grep tar.gz | while read $line;do echo "$line hereee";done
+ls /data/h36m | grep .tgz | while read $line;do echo "$line hereee";done
 # mv /data/h36m/** /data/h36m/raw
