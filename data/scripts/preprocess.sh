f0f67ecf5ebc8041d0a0d7c39b8ff0e48388b566
francopc <francopc@wolfram.com>
2022-07-24 00:12:16 -0500

    Getting hash of all files

--- data/scripts/preprocess.sh
+++ data/scripts/preprocess.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 mkdir -p /data/h36m/raw
+mkdir -p /data/h36m/uncompressed
 
 echo "raw folder"
-ls /data/h36m | grep .tgz | while read line; do echo "$line hereee";done
-ls /data/h36m | grep .tgz | while read line
+ls /data/h36m/raw | grep .tgz | while read line
 do
-    mv /data/h36m/${line} /data/h36m/raw/${line}
+    echo '{'
+    echo '  "file": "'"/data/h36m/raw/${line}"'",'
+    echo '  "sha": "'$(md5sum /data/h36m/raw/${line})'"'
+    echo '},'
 done
 # mv /data/h36m/** /data/h36m/raw
