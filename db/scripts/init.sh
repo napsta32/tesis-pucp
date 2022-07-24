39d314257463f246fbd38ee30a13f768f612bf21
francopc <francopc@wolfram.com>
2022-07-24 16:28:39 -0500

    Better logs

--- db/scripts/init.sh
+++ db/scripts/init.sh
@@ -2,7 +2,7 @@
 
 if [[ $(docker network ls --filter name="fpena_database_network" -q) ]]
 then
-    echo "Creating docker network"
+    echo "Creating docker network..."
     docker network create fpena_database_network
 fi
 
