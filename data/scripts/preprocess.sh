844489f4ee7b334f26d390e0d472027b4cc1d51a
francopc <francopc@wolfram.com>
2022-07-23 23:15:09 -0500

    List files line by line

--- data/scripts/preprocess.sh
+++ data/scripts/preprocess.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+rm -rf /data/h36m/raw
 mkdir -p /data/h36m/raw
+
 ls /data/h36m
 echo "raw folder"
-ls /data/h36m/raw
+ls /data/h36m | grep tar.gz | while read $line;do echo "$line hereee";done
 # mv /data/h36m/** /data/h36m/raw
