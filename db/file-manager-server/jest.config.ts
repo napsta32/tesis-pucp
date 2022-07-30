abf34d847e77575fc4448ee1e2f2695d367cf611
francopc <francopc@wolfram.com>
2022-07-30 02:27:08 -0500

    Fix jest config

--- db/file-manager-server/jest.config.ts
+++ db/file-manager-server/jest.config.ts
@@ -2,6 +2,8 @@ import type {Config} from '@jest/types';
 
 // Sync object
 const config: Config.InitialOptions = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
     verbose: true,
     collectCoverage: true,
     coverageThreshold: {
