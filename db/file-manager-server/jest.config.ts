f51b71b19dc95e421609e7795b3a7aa3fb5087f1
francopc <francopc@wolfram.com>
2022-07-30 03:54:10 -0500

    force exit

--- db/file-manager-server/jest.config.ts
+++ db/file-manager-server/jest.config.ts
@@ -14,6 +14,7 @@ const config: Config.InitialOptions = {
         }
     },
     rootDir: 'src',
+    forceExit: true,
     detectOpenHandles: true,
     setupFiles: [
         '<rootDir>/test-global-teardown.ts'
