3263a60668bef9bf44691317186ed62e87980251
francopc <francopc@wolfram.com>
2022-07-31 13:38:28 -0500

    reduce coverage

--- db/file-manager-server/jest.config.ts
+++ db/file-manager-server/jest.config.ts
@@ -13,12 +13,12 @@ const config: Config.InitialOptions = {
     coverageReporters: ['cobertura', 'text', 'lcov', 'html'],
     // coverageDirectory: 'reports/coverage',
     coverageThreshold: {
-        global: {lines: 80},
         './src/index.ts': {},
-        './src/api': {
-            branches: 100,
-            functions: 100,
-            lines: 100
+        './src/data-source.ts': {},
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80
         }
     },
     roots: ['<rootDir>/src'],
