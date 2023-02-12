a62325154a3fc1309aa05f6e223f5b32bdce8468
francopc <francopc@wolfram.com>
2023-02-12 15:30:38 -0500

    Use specific port for login auth

--- cicd/GDriveOperations/utils/GoogleCredentials.py
+++ cicd/GDriveOperations/utils/GoogleCredentials.py
@@ -28,7 +28,7 @@ def get_credentials():
 
             flow = InstalledAppFlow.from_client_secrets_file(
                 SECRETS_FILE, SCOPES)
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_local_server(port=7430)
         # Save the credentials for the next run
         with open(TOKEN_FILE, 'w') as token:
             token.write(creds.to_json())
