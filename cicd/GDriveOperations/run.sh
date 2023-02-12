a42a8ef9d2b07edc559c8a57bb3d7e7a10f29791
francopc <francopc@wolfram.com>
2023-02-12 14:42:11 -0500

    Fix run script

--- cicd/GDriveOperations/run.sh
+++ cicd/GDriveOperations/run.sh
@@ -4,6 +4,11 @@
 # ${1}: Action. It can be UpdateFile or UploadFile
 # ${2...}: Action arguments. See the python scripts for details
 
+action=${1}
+arguments=${@:2}
+
+echo "$action $arguments"
+
 docker build -t fpenac-upload-to-drive .
-docker run -v ${PWD}:/app fpenac-upload-to-drive python ${1}.py "${@:2}"
+docker run -v ${PWD}:/app fpenac-upload-to-drive sh -c "python ${action}.py ${arguments}"
 docker rmi fpenac-upload-to-drive
